766a0c74b2b916f74b1c7d8f76a00645b187468a
jujalu <jujalu@gmail.com>
2021-12-06 15:36:41 +0000

    Login funcionando

--- src/main.js
+++ src/main.js
@@ -2,7 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'https://8080-lavender-dove-00haudam.ws-us21.gitpod.io/spring-loja'
+
+axios.interceptors.request.use(config => {
+  if(store.state.token) {
+    config.headers.Authorization = store.state.token
+  }
+  return config
+})
+axios.interceptors.response.use(res => {
+    return res
+  }, error => {
+    if(error.response.status === 403) {
+      alert('Não autorizado!')
+    }
+    else if (error.response.status === 401) {
+      store.commit('logout')
+      router.push('/login')
+    }
+    throw error
+})
 Vue.config.productionTip = false
 
 new Vue({
