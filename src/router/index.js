2e688cdabe438b5e318d99f502dd35019f8b53a3
jujalu <jujalu@gmail.com>
2021-12-07 03:12:15 +0000

    router ok

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,26 @@ Vue.use(VueRouter)
     component: Home
   },
   {
+    path: "/marca",
+    name: "Marca",
+    component: () => import("../views/Marca.vue")
+  },
+  {
+    path: "/produto",
+    name: "Produto",
+    component: () => import("../views/Produto.vue")
+  },
+  {
+    path: "/venda",
+    name: "Venda",
+    component: () => import("../views/Venda.vue")
+  },
+  {
+    path: "/usuario",
+    name: "Usuario",
+    component: () => import("../views/Usuario.vue")
+  },
+  {
     path: "/login",
     name: "Login",
     component: () => import("../views/Login.vue")
