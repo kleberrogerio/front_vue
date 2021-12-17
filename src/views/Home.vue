3f8ba753d8870d9661b3957cd521c4600c62682b
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2021-12-17 02:18:39 +0000

    Alterando o Visual

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="text-center">
-        <font color="blue"><h1>Bem vindo!</h1></font>
+        <font color="red" v-if="usuario.autorizacao == 'admin'"><h1>Bem vindo Administrador!</h1></font>
+        <font color="green" v-if="usuario.autorizacao != 'admin'"><h1>Bem vindo!</h1></font>
         <font color="blue"><h3><p>Esse é um projeto em Vue que utiliza autenticação com JWT.</p></h3></font>
         <font color="blue"><h3><p>Projeto para o curso de Banco de Dados da FATEC - São José dos Campos.</p></h3></font>
         <body>
@@ -8,3 +9,16 @@
         </body>
     </div>
 </template>
+<script>
+  import { mapState } from 'vuex'
+  export default {
+    computed:{...mapState(['usuario'])},
+    created () {
+      if (!this.usuario) {
+        this.$router.push('/login')
+      }
+    }
+  }
+
+
+</script>
