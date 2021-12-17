04995240703e018211d930068179b04d1e71de5b
kleberrogerio <kleber.nascimento5@fatec.sp.gov.br>
2021-12-17 00:44:58 +0000

    Alterando cores HTML

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="text-center">
-        <h1>Bem vindo!</h1>
-        <p>Esse é um projeto em Vue que utiliza autenticação com JWT.</p>
-        <p>Projeto para o curso de Banco de Dados da FATEC - São José dos Campos.</p>
+        <font color="blue"><h1>Bem vindo!</h1></font>
+        <font color="blue"><h3><p>Esse é um projeto em Vue que utiliza autenticação com JWT.</p></h3></font>
+        <font color="blue"><h3><p>Projeto para o curso de Banco de Dados da FATEC - São José dos Campos.</p></h3></font>
         <body>
         <img alt="Vue logo" src="../assets/loja.jpg" width="500px" heigth="700px">       
         </body>
